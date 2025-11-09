2d381a507903e44c1eb8927fff811c309ac0772b
eyerisshe <irisshi832@gmail.com>
2025-11-09 06:42:23 +0000

    Add script to help with renaming data files

---
+++ scripts/rename_rdt_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+names=(PickCube-v1 PushCube-v1 StackCube-v1)
+
+for i in "${names[@]}"; do
+    DIR="../src/openpi/training/maniskill_data/demo_1k/${i}/motionplanning"
+    
+    # Find the .h5 file (assuming only one)
+    H5_FILE=$(ls "$DIR"/*.h5 2>/dev/null)
+    
+    if [ -n "$H5_FILE" ]; then
+        mv "$H5_FILE" "$DIR/${i}.h5"
+        echo "Renamed $H5_FILE to ${i}.h5"
+    else
+        echo "No .h5 file found in $DIR"
+    fi
+done
